5fb7faea83e0174cb012f6dedd9d8788406ffdab
Carolina Parisi <carolina.nunes@academiadecodigo.org>
2024-05-08 00:10:50 +0100

    feat: update movie card details with photo, genres, year and overview

--- js/view/detailsView.js
+++ js/view/detailsView.js
@@ -1,20 +1,50 @@
 async function show(movieInfo, movieImg) {
 	const image = await movieImg;
-	console.log(image);
-	console.log(movieInfo);
 
 	const filmContainer = document.getElementById("films-container");
 	filmContainer.innerHTML = "";
 
 	const supportDiv = document.createElement("div");
 	supportDiv.setAttribute("class", "support-details");
+
+	const supportMovieDetailsCard = document.createElement("div");
+	supportMovieDetailsCard.setAttribute("class", "support-details-card");
+
 	const movieCardInfo = document.createElement("div");
 	movieCardInfo.setAttribute("class", "details-card");
+
 	const img = document.createElement("img");
 	img.setAttribute("class", "film-img");
 	img.setAttribute("src", image.url);
 
+	const genres = document.createElement("div");
+	genres.setAttribute("class", "genresContainer");
+	let genreInitialString = "Genres: ";
+
+	const year = document.createElement("div");
+	year.setAttribute("class", "yearContainer");
+	year.innerText = "Released year: " + movieInfo.release_date.split("-")[0];
+
+	const overviewTitle = document.createElement("div");
+	overviewTitle.setAttribute("class", "overviewTitle");
+	overviewTitle.innerText = "Overview:";
+
+	const overviewText = document.createElement("div");
+	overviewText.setAttribute("class", "overviewText");
+	overviewText.innerText = movieInfo.overview;
+
+	movieInfo.genres.forEach((genre, index) => {
+		genreInitialString = genreInitialString.concat(genre.name + " ");
+	});
+
+	genres.innerText = genreInitialString;
+
 	movieCardInfo.append(img);
+	movieCardInfo.append(supportMovieDetailsCard);
+	supportMovieDetailsCard.append(genres);
+	supportMovieDetailsCard.append(year);
+	supportMovieDetailsCard.append(overviewTitle);
+	supportMovieDetailsCard.append(overviewText);
 	supportDiv.append(movieCardInfo);
 	filmContainer.append(supportDiv);
 }
